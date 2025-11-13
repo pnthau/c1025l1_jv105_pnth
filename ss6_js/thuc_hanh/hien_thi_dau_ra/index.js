4aef6a9e4421ef943e65ebb0e16fe7b5e0854368
pnthau <pnguyentrunghau.24@gmail.com>
2025-11-13 17:44:03 +0700

    nop_thuc_hanh_ss6_js

---
+++ ss6_js/thuc_hanh/hien_thi_dau_ra/index.js
@@ -0,0 +1,21 @@
+// #alert
+// function showMessage() {
+//     alert('xin chao!');
+// }
+
+
+//innerHTML
+// function showMessage(){
+//     document.getElementById('messageDiv').innerHTML = "Xin chao!";
+// }
+
+
+//document.write()
+// function showMessage() {
+//     document.writeln("Xin chao!");
+// }
+
+//console.log()
+function showMessage() {
+    console.log("xin chao!");
+}
---
+++ ss6_js/thuc_hanh/nhung_js/index.js
@@ -0,0 +1,6 @@
+function showMessage() {
+    alert("xin chao!");
+}
+
+var btn = document.getElementById('btnClick');
+btn.addEventListener('click', showMessage);
